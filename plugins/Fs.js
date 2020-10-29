be44af33367726e1a864321145e1f2caedc2cda4
F_ <hn.fspace@outlook.com>
2020-10-29 23:39:41 +0900

    chore(Fs): bump version to 0.2.0

--- plugins/Fs.js
+++ plugins/Fs.js
@@ -2,7 +2,7 @@
  * @target MZ
  * @plugindesc
  * 🚩 Basic function library for RPG Maker plugins.
- * Version: 0.1.2
+ * Version: 0.2.0
  * @author F_
  * @url https://github.com/f-space/rmmz-plugins
  * 
